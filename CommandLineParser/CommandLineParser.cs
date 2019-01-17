8a859f1f7482a6b68111b22905f17cabedd8e42e
RBryant57 <rwbryant57@outlook.com>
2019-01-16 23:12:32 -0800

    Removed exception.

--- CommandLineParser/CommandLineParser.cs
+++ CommandLineParser/CommandLineParser.cs
@@ -8,14 +8,25 @@ namespace CommandLineParser
 {
     public class CommandLineParser
     {
-        public Dictionary<string, string> Parse(string commandLine)
+        /// <summary>
+        /// In a production application these would be stored in a content management system and set through configuration.
+        /// </summary>
+        private const string FORMAT_MESSAGE =
+            "Invalid Format. Commands should be in the format of:\n --command name for uniary commands\n --command=parameter for commands that take parameters";
+
+        private const string SUCCESS_MESSAGE = "Success.";
+        
+        public Dictionary<string, string> Parse(string commandLine, out string message)
         {
             commandLine = commandLine.Trim();
+            var result = new Dictionary<string, string>();
 
             if (commandLine[0] != '-' && commandLine[1] != '-')
-                throw new InvalidCommandLineFormatException();
-
-            var result = new Dictionary<string, string>();
+            {
+                message = FORMAT_MESSAGE;
+                return result;
+            }
+            
             var indexArray = new List<int>();
 
             for (int i = 0; i < commandLine.Length; i++)
@@ -30,14 +41,29 @@ namespace CommandLineParser
 
                 var command = endIndex > indexArray.Count - 1 ? commandLine.Substring(indexArray[i]) : commandLine.Substring(indexArray[i], (indexArray[endIndex] - indexArray[i]));
                 command = TrimKey(command);
+                var key = String.Empty;
 
                 try
                 {
                     if (command.Contains("="))
                     {
                         var index = command.IndexOf("=");
-                        var key = command.Substring(0, index);
+                        key = command.Substring(0, index);
                         var value = command.Substring(index + 1);
+
+                        if (!ValidateCommand(key))
+                        {
+                            result.Clear();
+                            message = "The command: " + key + " is not recognized.";
+                            return result;
+                        }
+
+                        if (!ValidateParameter(key, value))
+                        {
+                            result.Clear();
+                            message = "The parameter: " + value + " is not recognized as a parameter for the command: " + key + ".";
+                            return result;
+                        }
 
                         result.Add(key.Trim(), value.Trim());
                     }
@@ -48,10 +74,13 @@ namespace CommandLineParser
                 }
                 catch (ArgumentException ex)
                 {
-                    throw new InvalidCommandLineFormatException();
+                    message = "The command: " + key + " has already been invoked.";
+                    result.Clear();
+                    return result;
                 }
             }
 
+            message = SUCCESS_MESSAGE;
             return result;
         }
 
@@ -59,10 +88,29 @@ namespace CommandLineParser
         {
             return key.Substring(2, key.Length - 2);
         }
+
+        /// <summary>
+        /// In a production application, this would validate a command against a
+        /// set of accepted values.
+        /// </summary>
+        /// <param name="command">The command to be validated.</param>
+        /// <returns>Indicates whether or not the command is valid.</returns>
+        private bool ValidateCommand(string command)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// In a production application, this would validate a command parameter against a
+        /// set of accepted values.
+        /// </summary>
+        /// <param name="command">The command against which to validate the parameter.</param>
+        /// <param name="parameter">The parameter to be validated.</param>
+        /// <returns>Indicates whether or not the parameter is valid.</returns>
+        private bool ValidateParameter(string command, string parameter)
+        {
+            return true;
+        }
     }
 
-    public class InvalidCommandLineFormatException : Exception
-    {
-
-    }
 }
--- CommandLineParserTests/CommandLineParserTest.cs
+++ CommandLineParserTests/CommandLineParserTest.cs
@@ -14,8 +14,9 @@ namespace CommandLineParserTests
             var stringUnderTest = "--key --anotherKey=value --yetAnotherKey --StillAnotherKey=anotherValue";
             var commandLineParser = new CommandLineParser.CommandLineParser();
 
-            var result = commandLineParser.Parse(stringUnderTest);
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
             Assert.IsTrue(result.Count == 4);
+            Assert.AreEqual(message, "Success.");
         }
 
         [TestMethod]
@@ -24,8 +25,9 @@ namespace CommandLineParserTests
             var stringUnderTest = "--key--anotherKey=value--yetAnotherKey--StillAnotherKey=anotherValue";
             var commandLineParser = new CommandLineParser.CommandLineParser();
 
-            var result = commandLineParser.Parse(stringUnderTest);
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
             Assert.IsTrue(result.Count == 4);
+            Assert.AreEqual(message, "Success.");
         }
 
         [TestMethod]
@@ -34,8 +36,9 @@ namespace CommandLineParserTests
             var stringUnderTest = "--key          --anotherKey=value    --   yetAnotherKey           --StillAnotherKey=anotherValue";
             var commandLineParser = new CommandLineParser.CommandLineParser();
 
-            var result = commandLineParser.Parse(stringUnderTest);
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
             Assert.IsTrue(result.Count == 4);
+            Assert.AreEqual(message, "Success.");
         }
 
         [TestMethod]
@@ -44,8 +47,9 @@ namespace CommandLineParserTests
             var stringUnderTest = "--key --anotherKey=value --yetAnotherKey";
             var commandLineParser = new CommandLineParser.CommandLineParser();
 
-            var result = commandLineParser.Parse(stringUnderTest);
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
             Assert.IsFalse(result.Keys.Any(k => k.Contains("--")));
+            Assert.AreEqual(message, "Success.");
         }
 
         [TestMethod]
@@ -54,8 +58,9 @@ namespace CommandLineParserTests
             var stringUnderTest = "--key=value";
             var commandLineParser = new CommandLineParser.CommandLineParser();
 
-            var result = commandLineParser.Parse(stringUnderTest);
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
             Assert.AreEqual(result.Keys.First(), "key");
+            Assert.AreEqual(message, "Success.");
         }
 
         [TestMethod]
@@ -64,29 +69,53 @@ namespace CommandLineParserTests
             var stringUnderTest = "--key=value";
             var commandLineParser = new CommandLineParser.CommandLineParser();
 
-            var result = commandLineParser.Parse(stringUnderTest);
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
             Assert.AreEqual(result.Values.First(), "value");
+            Assert.AreEqual(message, "Success.");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidCommandLineFormatException))]
+        public void ValidCommandsShouldBeParsedCorrectly()
+        {
+            var stringUnderTest = "--key=value";
+            var commandLineParser = new CommandLineParser.CommandLineParser();
+
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
+            Assert.IsTrue(result.Count > 0);
+            Assert.AreEqual(message, "Success.");
+        }
+
+        [TestMethod]
+        public void ValidParametersShouldBeParsedCorrectly()
+        {
+            var stringUnderTest = "--key=value";
+            var commandLineParser = new CommandLineParser.CommandLineParser();
+
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
+            Assert.IsTrue(result.Count > 0);
+            Assert.AreEqual(message, "Success.");
+        }
+
+        [TestMethod]
         public void DuplicateKeysShouldNotParseCorrectly()
         {
             var stringUnderTest = "--key=value --key=value";
             var commandLineParser = new CommandLineParser.CommandLineParser();
 
-            var result = commandLineParser.Parse(stringUnderTest);
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
+            Assert.IsTrue(result.Count == 0);
+            Assert.AreEqual(message, "The command: key has already been invoked.");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidCommandLineFormatException))]
         public void InvalidCommandLineShouldNotParseCorrectly()
         {
             var stringUnderTest = "keyvalue=valuesomestuff";
             var commandLineParser = new CommandLineParser.CommandLineParser();
 
-            var result = commandLineParser.Parse(stringUnderTest);
-
+            var result = commandLineParser.Parse(stringUnderTest, out var message);
+            Assert.IsTrue(result.Count == 0);
+            Assert.AreEqual(message, "Invalid Format. Commands should be in the format of:\n --command name for uniary commands\n --command=parameter for commands that take parameters");
         }
     }
 }
